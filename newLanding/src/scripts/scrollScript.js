c11454f128d09614451c3f0780f59ae44ca9de11
weeered <qturtle22@gmail.com>
2025-01-28 14:17:25 +0300

    animation optimization

--- newLanding/src/scripts/scrollScript.js
+++ newLanding/src/scripts/scrollScript.js
@@ -57,7 +57,7 @@ headerDynamic.style.boxShadow = "0px 1.5px 3px #00000040";
 headerDynamic.style.position = "fixed";
 headerDynamic.style.top = 0;
 headerDynamic.style.transform = "translate(0, calc(-100% - 3px))";
-headerDynamic.style.borderRadius = "0 0 100% 100%";
+//headerDynamic.style.borderRadius = "0 0 100% 100%";
 headerDynamic.style.zIndex = "13";
 
 const sections = [
@@ -78,39 +78,39 @@ if (window.getComputedStyle(burgerButtons[0], null).display != "none") {
 			headerAnimationLayer1.style.transition =
 				"transform 0.25s ease-in-out, border-radius 0.25s ease-in-out, background-color 0s 0.5s";
 			headerAnimationLayer1.style.transform = "translate(0, 0)";
-			headerAnimationLayer1.style.borderRadius = 0;
+			//headerAnimationLayer1.style.borderRadius = 0;
 			headerAnimationLayer1.style.backgroundColor = "transparent";
 
 			headerAnimationLayer2.style.transition =
 				"transform 0.25s 0.125s ease-in-out, border-radius 0.25s 0.125s ease-in-out, background-color 0s 0.5s";
 			headerAnimationLayer2.style.transform = "translate(0, 0)";
-			headerAnimationLayer2.style.borderRadius = 0;
+			//headerAnimationLayer2.style.borderRadius = 0;
 			headerAnimationLayer2.style.backgroundColor = "transparent";
 
 			headerDynamic.style.transition =
 				"transform 0.25s 0.25s ease-in-out, border-radius 0.25s 0.25s ease-in-out, background-color 1s 0.5s ease-in-out, box-shadow 1s 0.5s ease-in-out";
 			headerDynamic.style.transform = "translate(0, 0)";
-			headerDynamic.style.borderRadius = 0;
+			//headerDynamic.style.borderRadius = 0;
 			headerDynamic.style.backgroundColor = "#342e8cbf";
 			headerDynamic.style.boxShadow = "0px 1.5px 3px #342e8c40";
 		} else {
 			headerDynamic.style.transition =
 				"transform 0.25s ease-in-out, border-radius 0.25s ease-in-out, background-color 0.125s 0s ease-in-out, box-shadow 0.125s 0s ease-in-out";
 			headerDynamic.style.transform = "translate(0, calc(-100% - 3px))";
-			headerDynamic.style.borderRadius = "0 0 100% 100%";
+			//headerDynamic.style.borderRadius = "0 0 100% 100%";
 			headerDynamic.style.backgroundColor = "#342e8c";
 			headerDynamic.style.boxShadow = "0px 1.5px 3px #00000040";
 
 			headerAnimationLayer2.style.transition =
 				"transform 0.25s 0.125s ease-in-out, border-radius 0.25s 0.125s ease-in-out, background-color 0s 0s";
 			headerAnimationLayer2.style.transform = "translate(0, calc(-100% - 3px))";
-			headerAnimationLayer2.style.borderRadius = "0 0 100% 100%";
+			//headerAnimationLayer2.style.borderRadius = "0 0 100% 100%";
 			headerAnimationLayer2.style.backgroundColor = "#d3281d";
 
 			headerAnimationLayer1.style.transition =
 				"transform 0.25s 0.25s ease-in-out, border-radius 0.25s 0.25s ease-in-out, background-color 0s 0s";
 			headerAnimationLayer1.style.transform = "translate(0, calc(-100% - 3px))";
-			headerAnimationLayer1.style.borderRadius = "0 0 100% 100%";
+			//headerAnimationLayer1.style.borderRadius = "0 0 100% 100%";
 			headerAnimationLayer1.style.backgroundColor = "#f19001";
 		}
 	});
@@ -122,39 +122,39 @@ if (window.getComputedStyle(burgerButtons[0], null).display != "none") {
 			headerAnimationLayer1.style.transition =
 				"transform 0.25s ease-in-out, border-radius 0.25s ease-in-out, background-color 0s 0.5s";
 			headerAnimationLayer1.style.transform = "translate(0, 0)";
-			headerAnimationLayer1.style.borderRadius = 0;
+			//headerAnimationLayer1.style.borderRadius = 0;
 			headerAnimationLayer1.style.backgroundColor = "transparent";
 
 			headerAnimationLayer2.style.transition =
 				"transform 0.25s 0.125s ease-in-out, border-radius 0.25s 0.125s ease-in-out, background-color 0s 0.5s";
 			headerAnimationLayer2.style.transform = "translate(0, 0)";
-			headerAnimationLayer2.style.borderRadius = 0;
+			//headerAnimationLayer2.style.borderRadius = 0;
 			headerAnimationLayer2.style.backgroundColor = "transparent";
 
 			headerDynamic.style.transition =
 				"transform 0.25s 0.25s ease-in-out, border-radius 0.25s 0.25s ease-in-out, background-color 1s 0.5s ease-in-out, box-shadow 1s 0.5s ease-in-out";
 			headerDynamic.style.transform = "translate(0, 0)";
-			headerDynamic.style.borderRadius = 0;
+			//headerDynamic.style.borderRadius = 0;
 			headerDynamic.style.backgroundColor = "#342e8cbf";
 			headerDynamic.style.boxShadow = "0px 1.5px 3px #342e8c40";
 		} else {
 			headerDynamic.style.transition =
 				"transform 0.25s ease-in-out, border-radius 0.25s ease-in-out, background-color 0.125s 0s ease-in-out, box-shadow 0.125s 0s ease-in-out";
 			headerDynamic.style.transform = "translate(0, calc(-100% - 3px))";
-			headerDynamic.style.borderRadius = "0 0 100% 100%";
+			//headerDynamic.style.borderRadius = "0 0 100% 100%";
 			headerDynamic.style.backgroundColor = "#342e8c";
 			headerDynamic.style.boxShadow = "0px 1.5px 3px #00000040";
 
 			headerAnimationLayer2.style.transition =
 				"transform 0.25s 0.125s ease-in-out, border-radius 0.25s 0.125s ease-in-out, background-color 0s 0s";
 			headerAnimationLayer2.style.transform = "translate(0, calc(-100% - 3px))";
-			headerAnimationLayer2.style.borderRadius = "0 0 100% 100%";
+			//headerAnimationLayer2.style.borderRadius = "0 0 100% 100%";
 			headerAnimationLayer2.style.backgroundColor = "#d3281d";
 
 			headerAnimationLayer1.style.transition =
 				"transform 0.25s 0.25s ease-in-out, border-radius 0.25s 0.25s ease-in-out, background-color 0s 0s";
 			headerAnimationLayer1.style.transform = "translate(0, calc(-100% - 3px))";
-			headerAnimationLayer1.style.borderRadius = "0 0 100% 100%";
+			//headerAnimationLayer1.style.borderRadius = "0 0 100% 100%";
 			headerAnimationLayer1.style.backgroundColor = "#f19001";
 		}
 	});
