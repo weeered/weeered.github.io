1cc8c01d2d24b4a76749a9e5f706a231da8ee41e
weeered <qturtle22@gmail.com>
2025-09-19 18:57:03 +0300

    update

---
+++ placeholderName/src/mailInfo.php
@@ -0,0 +1,43 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    // Retrieve the form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $tel = $_POST["tel"];
+    $message = $_POST["message"];
+
+    $messageComposed =
+        "От: $name" .
+        "\r\n" .
+        "Почта: $email" .
+        "\r\n" .
+        "Телефон: $tel" .
+        "\r\n" .
+        "Сообщение: " .
+        "\r\n" .
+        "$message";
+
+    $headers = "Content-type: text/html; charset=UTF-8" . "\r\n";
+
+    $messageHTML = nl2br($messageComposed);
+
+    // Create a method to send email
+
+    if (
+        mail(
+            "info@culinarycup.bcmagazine.by",
+            "Message Form",
+            $messageHTML,
+            $headers,
+        )
+    ) {
+        header("Location: ./pages/formSuccess.html");
+        die();
+    } else {
+        header("Location: ./pages/formFailure.html");
+        die();
+    }
+}
+
+?>
