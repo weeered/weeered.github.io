d5f6d1766c8ac7fab6c5b9bfe80d08eef07be6ad
weeered <qturtle22@gmail.com>
2025-10-29 20:34:22 +0300

    fix

--- ychampionship_cond/src/mailInfo.php
+++ ychampionship_cond/src/mailInfo.php
@@ -31,7 +31,7 @@ if (isset($_POST["submit"])) {
     if (
         mail(
             "info@culinarycup.bcmagazine.by",
-            "Формы cofecart.bcmagazine.by",
+            "Формы confecart.bcmagazine.by",
             $messageHTML,
             $headers,
         )
